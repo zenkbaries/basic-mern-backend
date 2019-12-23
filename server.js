589999d9d8af841182c3a3a5facc305cc5434825
John Towery <kbaries@gmail.com>
2019-12-23 08:09:17 -0700

    add middleware

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import bodyParser from 'bpdy-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.listen (
     PORT,
     () => {
-        console.log('Listen on port ' + PORT)
+        console.log('Listen on port: ' + PORT)
     }
 );
