ce7a60f2f85ed2cea991f0844487b5942f2fa462
John Towery <kbaries@gmail.com>
2019-12-24 04:37:29 -0700

    add express instance, bodyParser,cors,mongoos

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const express = require( 'express');
- 
-
-const bodyParser = require( 'body-parser');
-const cors = require( 'cors');
-
+const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/todos',
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
+);
+
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen (
     PORT,
